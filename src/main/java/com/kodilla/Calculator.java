56a50353bee27dd6b714bf6a28846eb00f181a50
sekrez <sebastian.w.krezel@gmail.com>
2017-12-10 00:29:40 +0100

    First Commit

---
+++ src/main/java/com/kodilla/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+// test clasy calculator
+class Calculator {
+    public void demo() {
+        System.out.println("Demo kalkulatora");
+    }
+
+
+    public static void main(String args[]) {
+        Calculator calculator = new Calculator();
+        calculator.demo();
+    }
+}
